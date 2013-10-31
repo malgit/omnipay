5238277bdf1cf44db9ec81e3d3010d817a7dae1b
malgit <Email>
2013-10-31 21:00:27 +0000

    Added GlobalIris Gateway

---
+++ src/Omnipay/GlobalIris/Gateway.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Omnipay\GlobalIris;
+
+use Omnipay\Common\AbstractGateway;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Gateway
+ *
+ * 
+ */
+class Gateway extends AbstractGateway {
+    
+    public function getDefaultParameters() {
+        
+    }
+
+    public function getName() {
+        
+    }
+
+    public function purchase(array $parameters = array()) {
+        
+    }   
+}
+
+?>
